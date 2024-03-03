d3a7870791ab919e472f29692363c4e6bad331ca
saallyyang <161184485+saallyyang@users.noreply.github.com>
2024-03-03 12:59:42 +1100

    Added javascript

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(submit) {
+  event.preventDefault();
+  let searchInput = document.querySelector("search-form-input");
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
