0d2195947f70616ef310be6db79a0cfe4cbe3339
saallyyang <161184485+saallyyang@users.noreply.github.com>
2024-03-03 13:14:18 +1100

    Fixed javascript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function handleSearchSubmit(submit) {
-  event.preventDefault();
-  let searchInput = document.querySelector("search-form-input");
+  submit.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = searchInput.value;
 }
