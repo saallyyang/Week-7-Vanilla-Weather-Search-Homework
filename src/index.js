dad50d312d38378e69708e7cd4ec433885689f02
saallyyang <161184485+saallyyang@users.noreply.github.com>
2024-03-03 15:05:54 +1100

    Insert forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -53,7 +53,31 @@ function handleSearchSubmit(submit) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+        <div class="forecast-day">
+        <div class="forecast-date">${day}</div>
+        <div class="forecast-icon">🌤️</div>
+        <div class="forecast-temps">
+        <div class="forecast-temp"><strong>15º</strong></div>
+        <div class="forecast-temp">9º</div>
+        </div>
+        </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
+displayForecast();
